31a51d89e3911bee8d99393db8e3f0d7082f58e6
Sagar <sagarsingh.cs1996@gmail.com>
2018-01-14 16:29:12 +0530

    initial commit

---
+++ chatbot.js
@@ -0,0 +1,50 @@
+
+function setup(params) {
+    noCanvas();
+
+    let speech = new p5.Speech();
+    let speechRec = new p5.SpeechRec('en-US',gotSpeech);
+    let continuous = true;
+    let interim = false;
+    speechRec.start(continuous,interim);
+
+    var bot = new RiveScript();
+
+    bot.loadFile("brain.rive", brainReady, brainError);
+
+    function brainReady() {
+        console.log('chatbot ready!');
+        bot.sortReplies();
+        // let num = floor(random(10))+1;
+        // let reply = bot.reply('local-user','set '+num);
+        // console.log(num);
+    }
+
+    function brainError() {
+        console.log('chatbot Error!');
+    }
+
+    // let button = select('#submit');
+    let user_input = select('#user_input');
+    let output = select('#output');
+
+    // button.mousePressed(chat);
+
+    function gotSpeech() {
+        if(speechRec.resultValue){
+            let input = speechRec.resultValue;
+            console.log(input);
+            user_input.html(input);
+            let reply = bot.reply("local-user", input);
+            speech.speak(reply);
+            output.html(reply);
+        }        
+    }
+
+    // function chat() {
+    //     let input = user_input.value();
+    //     let reply = bot.reply("local-user",input);
+    //     speech.speak(reply);
+    //     output.html(reply);
+    // }
+}
